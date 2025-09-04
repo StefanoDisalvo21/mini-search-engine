5da5870f4c256aa7143d63354a4eea5731da9bb0
StefanoDisalvo21 <stefanodisalvo21@gmail.com>
2025-09-04 17:20:38 +0200

    Feature: added and tested checks on the data folder

--- src/document.cpp
+++ src/document.cpp
@@ -11,9 +11,10 @@ using namespace std;
 namespace fs = std::filesystem;
 
 vector<Document> DomLoad::read_docs(const string& path){
+    vector<Document> ds;
     if(!fs::exists(path)){
         throw invalid_argument("The path does not exist");
     }
-
+    return ds;
 }
 
--- tests/doc_test.cpp
+++ tests/doc_test.cpp
@@ -1,2 +1,10 @@
 #include <gtest/gtest.h>
+#include "document.hpp"
 using namespace std;
+
+TEST(Load_Test,first_test){
+    DomLoad dom_obj;
+    string ds="../../random_name";
+    EXPECT_THROW(dom_obj.read_docs(ds),invalid_argument);
+
+}
