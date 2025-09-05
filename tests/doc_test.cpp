e6f6d007a501ff276af691b999f36887217c0070
StefanoDisalvo21 <stefanodisalvo21@gmail.com>
2025-09-05 13:25:05 +0200

    Tests: Tested the loading operation of the data

--- tests/doc_test.cpp
+++ tests/doc_test.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <vector>
+#include <iostream>
 #include "document.hpp"
 using namespace std;
 
@@ -13,4 +15,29 @@ TEST(Load_Test,second_test){
     string ds="../../README.md";
     EXPECT_THROW(dom_obj.read_docs(ds),invalid_argument);
 
+}
+
+TEST(Load_Test,third_test){
+    DomLoad dom_obj;
+    string ds="../../data";
+    vector<Document> docs = dom_obj.read_docs(ds);
+    sort(docs.begin(),docs.end());
+    vector<string> names;
+    vector<string> expected_names={"art1.txt","art2.txt","art3.txt","art4.txt"};
+    for(int i=0;i<docs.size();i++){
+        names.push_back(docs[i].get_file_name());
+    }
+    EXPECT_EQ(names,expected_names);
+}
+TEST(Load_Test,fourth_test){
+    DomLoad dom_obj;
+    Document doc("art1.txt","hi");
+    Document doc1("art2.txt","hi");
+    Document doc2("art3.txt","hi");
+    Document doc3("art4.txt","hi");
+    string ds="../../data";
+    vector<Document> vt = dom_obj.read_docs(ds);
+    vector<Document> expected_vector={doc,doc1,doc2,doc3};
+    sort(vt.begin(),vt.end());
+    EXPECT_EQ(vt,expected_vector);
 }
