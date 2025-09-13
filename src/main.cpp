4bdc0f28f6ed4889deb763c61abb3365a8011dea
StefanoDisalvo21 <stefanodisalvo21@gmail.com>
2025-09-13 11:02:08 +0200

    Feature: implemented in the main the tokens extrapolation

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@ int main(){
     int choice;
     DomLoad loader;
     const string data_information = "../../data";
-    vector<Document> docs=loader.load_data(data_information);
+    vector<Document> data_vector=loader.load_data(data_information);
+    vector<vector<string>> tokens;
+    for(const auto& docs :data_vector){
+        tokens.push_back(docs.get_tokens());
+    }
     return 0;
 }
