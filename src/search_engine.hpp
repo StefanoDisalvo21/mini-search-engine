8be7bd689565ea3b4016261beec81d2b992d506b
StefanoDisalvo21 <stefanodisalvo21@gmail.com>
2025-09-23 18:19:19 +0200

    refactor: declaration of new method to calculate the score

--- src/search_engine.hpp
+++ src/search_engine.hpp
@@ -16,4 +16,5 @@ class SearchEngine{
         const unordered_map<string,unordered_map<string,int>>& get_index() const {return index;};
         void build_index(vector<Document>& data_vector);
         vector<pair<string,double>> search(string& query,vector<Document>& data_vector);
+        vector<pair<string,double>>calc_score(string& quert,vector<Document>&data_vector);
 };
