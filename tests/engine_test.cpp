a07992f2ca76d82705a1c6fe5affb7852d6a699a
StefanoDisalvo21 <stefanodisalvo21@gmail.com>
2025-09-23 18:53:55 +0200

    tests: add test on evaluation

--- tests/engine_test.cpp
+++ tests/engine_test.cpp
@@ -82,15 +82,44 @@ TEST(Score_Test, first_test){
     SearchEngine index_try;
     index_try.build_index(vector_docs);
     unordered_map<string, unordered_map<string,int>> index=index_try.get_index();
-    for (auto& [word, doc_map] : index) {
-        cout << word << ": ";
-        for (auto& [file, occ] : doc_map) std::cout << file << "(" << occ << ") ";
-            cout << "\n";
-    }
     string test_search="Cat Elephant";
     vector<pair<string,double>> results=index_try.search(test_search,vector_docs);
     double expected_score=0.274;
     EXPECT_EQ(results[0].first, "doc3.txt"); 
+    EXPECT_NEAR(results[0].second, expected_score, 1e-3);
+
+}
+
+TEST(Score_Test, Second_Test){
+    Document docs("doc1.txt", "Ai is better than math, but math is required to learn AI");
+    Document docs2("doc2.txt", "Nothing is cool such as AI, but I think math is cool too");
+    Document docs3("doc3.txt", "AI is cool, but learning it could be painful");
+    vector<Document> vector_docs;
+    string cont= docs.get_file_content();
+    icu::UnicodeString norm_string = helpers::normalization(cont);
+    docs.set_normalized_string(norm_string);
+    vector<string> tokens= helpers::tokenization(norm_string);
+    docs.set_tokens(tokens);
+    string cont2= docs2.get_file_content();
+    icu::UnicodeString norm_string2 = helpers::normalization(cont2);
+    docs2.set_normalized_string(norm_string2);
+    vector<string> tokens2= helpers::tokenization(norm_string2);
+    docs2.set_tokens(tokens2);
+    string cont3= docs3.get_file_content();
+    icu::UnicodeString norm_string3 = helpers::normalization(cont3);
+    docs3.set_normalized_string(norm_string3);
+    vector<string> tokens3= helpers::tokenization(norm_string3);
+    docs3.set_tokens(tokens3);
+    vector_docs.push_back(docs);
+    vector_docs.push_back(docs2);
+    vector_docs.push_back(docs3);
+    SearchEngine index_try;
+    index_try.build_index(vector_docs);
+    unordered_map<string, unordered_map<string,int>> index=index_try.get_index();
+    string test_search="AI";
+    vector<pair<string,double>> results=index_try.search(test_search,vector_docs);
+    double expected_score=0.0502;
+    EXPECT_EQ(results[0].first, "doc1.txt"); 
     EXPECT_NEAR(results[0].second, expected_score, 1e-3);
 
 }
